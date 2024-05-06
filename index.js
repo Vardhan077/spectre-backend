4ff63b2ee26a3190cf855eabf9318c503f9bd78c
Vardhan077 <132035215+Vardhan077@users.noreply.github.com>
2024-05-06 21:39:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const salt = 10;
 const app =  express();
 app.use(express.json());
 app.use(cors({
-    origin:['http://localhost:3000'],
+    origin:['https://shoppingappnew.netlify.app'],
     methods:["POST","GET"],
     credentials:true
 }));
